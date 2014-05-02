fc0349b91cf85bdf8938f2e82f693d6478004726
Felipe Andrade <felipe_fac_8@hotmail.com>
2014-05-02 09:30:13 -0300

    Incompleta

--- Lista 3/questao09.cpp
+++ Lista 3/questao09.cpp
@@ -1,23 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 
 int main () 
 {
 	int i, j=1,k=1;
 	
 	for (i=1;i <= 100;i++)
-		printf("\t%d",i);
+		{
+			
+			printf("%d\t",i);
+			usleep( 100000);
+		}
 	printf("\n");
 	while (j <= 100)
 		{
-			printf("\t%d",j);
+			printf("%d\t",j);
 			j++;
+			usleep( 100000);
 		}
 	printf("\n");
 	do
 	{
-		printf("\t%d",k);
+		printf("%d\t",k);
 		k++;
-		
+		usleep( 100000);
 		
 	} while(k<=100);
+	system("pause");
+	return 0;
 }
